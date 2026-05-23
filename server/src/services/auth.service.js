e8c8a8b99ec3876e4f70bf674c2388cdd679b3df
DanishIslam <danishislam328@gmail.com>
2026-05-23 15:18:19 +0530

    debug

--- server/src/services/auth.service.js
+++ server/src/services/auth.service.js
@@ -1,7 +1,7 @@
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
 import crypto from "crypto";
-import User from "../models/User.js"
+import User from "../models/User.js";
 
 export const signupService = async (data) => {
 
